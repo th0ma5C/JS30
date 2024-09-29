3cd703b7c3bd11d63e8faf5b0030f1596c7f8be2
th0ma5C <thomas29111@gmail.com>
2024-09-29 16:28:24 +0800

    0929 19 practice

--- src/19/index.js
+++ src/19/index.js
@@ -10,7 +10,7 @@ var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
     return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
 };
-var _WebCam_videoEl, _WebCam_strip;
+var _WebCam_videoEl, _WebCam_strip, _WebCam_rgbState;
 const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const strip = document.querySelector('.strip');
@@ -19,10 +19,12 @@ class WebCam {
     constructor(selector) {
         _WebCam_videoEl.set(this, void 0);
         _WebCam_strip.set(this, document.querySelector('.strip'));
+        _WebCam_rgbState.set(this, { temp: 0, tint: 0, filter: 'origin' });
         __classPrivateFieldSet(this, _WebCam_videoEl, document.querySelector(selector), "f");
         this.initCam();
         // this.#videoEl.addEventListener('loadedmetadata', () => {
         // })
+        // this.getRGBInput()
     }
     initCam() {
         navigator.mediaDevices.getUserMedia({
@@ -35,18 +37,18 @@ class WebCam {
         });
     }
     setIntoCanvas(canvas) {
-        const ctx = canvas.getContext('2d');
+        const ctx = canvas.getContext('2d', { willReadFrequently: true });
         if (!ctx)
             throw Error('missing canvas ctx');
         const width = __classPrivateFieldGet(this, _WebCam_videoEl, "f").videoWidth;
         const height = __classPrivateFieldGet(this, _WebCam_videoEl, "f").videoHeight;
         canvas.width = width;
         canvas.height = height;
-        return setInterval(() => {
-            ctx.drawImage(__classPrivateFieldGet(this, _WebCam_videoEl, "f"), 0, 0, width, height);
-            // let pixels = ctx.getImageData(0, 0, width, height);
-            // ctx.putImageData(pixels, 0, 0);
-        }, 16);
+        ctx.drawImage(__classPrivateFieldGet(this, _WebCam_videoEl, "f"), 0, 0, width, height);
+        let pixels = ctx.getImageData(0, 0, width, height);
+        pixels = this.setColor(pixels);
+        ctx.putImageData(pixels, 0, 0);
+        requestAnimationFrame(this.setIntoCanvas.bind(this, canvas));
     }
     takePhoto(canvas) {
         const data = canvas.toDataURL('image/jpeg');
@@ -57,15 +59,88 @@ class WebCam {
         __classPrivateFieldGet(this, _WebCam_strip, "f").insertBefore(link, __classPrivateFieldGet(this, _WebCam_strip, "f").firstChild);
     }
     getColorInput() {
+        const rgbContainer = document.querySelector('.rgb');
+        rgbContainer.querySelectorAll('input').forEach(input => {
+            const elName = input.name;
+            const val = Number(input.value);
+            __classPrivateFieldGet(this, _WebCam_rgbState, "f")[elName] = val;
+        });
+        const filter = document.querySelector('.imgFilter');
+        filter.querySelectorAll('input').forEach(input => {
+            if (!input.checked)
+                return;
+            const val = input.value;
+            __classPrivateFieldGet(this, _WebCam_rgbState, "f").filter = val;
+        });
     }
-    setColorInput() {
+    adjustState(r, g, b) {
+        const { temp, tint } = __classPrivateFieldGet(this, _WebCam_rgbState, "f");
+        const newR = r + temp * 50;
+        const newB = b - temp * 50;
+        const newG = g + tint * 50;
+        return [
+            Math.max(0, Math.min(255, newR)),
+            Math.max(0, Math.min(255, newG)),
+            Math.max(0, Math.min(255, newB))
+        ];
+    }
+    grayScale(imageData) {
+        for (let i = 0; i < imageData.data.length; i += 4) {
+            const red = imageData.data[i];
+            const green = imageData.data[i + 1];
+            const blue = imageData.data[i + 2];
+            const avg = (red + green + blue) / 3;
+            imageData.data[i] = avg; // Red
+            imageData.data[i + 1] = avg; // Green
+            imageData.data[i + 2] = avg; // Blue
+        }
+    }
+    inverted(imageData) {
+        for (let i = 0; i < imageData.data.length; i += 4) {
+            imageData.data[i] = 255 - imageData.data[i]; // Red
+            imageData.data[i + 1] = 255 - imageData.data[i + 1]; // Green
+            imageData.data[i + 2] = 255 - imageData.data[i + 2]; // Blue
+        }
+    }
+    applyFilter(imageData) {
+        switch (__classPrivateFieldGet(this, _WebCam_rgbState, "f").filter) {
+            case 'grayscale':
+                this.grayScale(imageData);
+                break;
+            case 'inverted':
+                this.inverted(imageData);
+                break;
+            default:
+                break;
+        }
+    }
+    setColor(target) {
+        this.getColorInput();
+        for (let i = 0; i < target.data.length; i += 4) {
+            const red = target.data[i];
+            const green = target.data[i + 1];
+            const blue = target.data[i + 2];
+            const [newR, newG, newB] = this.adjustState(red, green, blue);
+            target.data[i] = newR;
+            target.data[i + 1] = newG;
+            target.data[i + 2] = newB;
+        }
+        this.applyFilter(target);
+        return target;
     }
 }
-_WebCam_videoEl = new WeakMap(), _WebCam_strip = new WeakMap();
-// const Cam = new WebCam('.player');
+_WebCam_videoEl = new WeakMap(), _WebCam_strip = new WeakMap(), _WebCam_rgbState = new WeakMap();
+const Cam = new WebCam('.player');
 video.addEventListener('loadedmetadata', () => {
-    // Cam.setIntoCanvas(canvas);
+    Cam.setIntoCanvas(canvas);
 });
 function takePhoto() {
     // Cam.takePhoto(canvas);
 }
+const rgbContainer = document.querySelector('.rgb');
+console.dir(rgbContainer);
+/**
+ * 實作功能
+ * webcam投影
+ * 並改變影像輸出
+*/ 
--- src/19/index.ts
+++ src/19/index.ts
@@ -4,16 +4,22 @@ const canvas = document.querySelector('.photo') as HTMLCanvasElement;
 const strip = document.querySelector('.strip') as HTMLElement;
 const snap = document.querySelector('.snap') as HTMLAudioElement;
 
+interface RgbState {
+    temp: number,
+    tint: number,
+    filter: string
+}
 class WebCam {
     #videoEl;
     #strip = document.querySelector('.strip') as HTMLElement;
-
+    #rgbState: RgbState = { temp: 0, tint: 0, filter: 'origin' };
 
     constructor(selector: string) {
         this.#videoEl = document.querySelector(selector) as HTMLVideoElement;
         this.initCam();
         // this.#videoEl.addEventListener('loadedmetadata', () => {
         // })
+        // this.getRGBInput()
     }
 
     initCam() {
@@ -22,13 +28,14 @@ class WebCam {
         }).then((stream) => {
             this.#videoEl.srcObject = stream;
             this.#videoEl.play();
+
         }).catch((err) => {
             console.log(err);
         })
     }
 
     setIntoCanvas(canvas: HTMLCanvasElement) {
-        const ctx = canvas.getContext('2d');
+        const ctx = canvas.getContext('2d', { willReadFrequently: true });
         if (!ctx) throw Error('missing canvas ctx');
 
         const width = this.#videoEl.videoWidth;
@@ -36,11 +43,12 @@ class WebCam {
         canvas.width = width;
         canvas.height = height;
 
-        return setInterval(() => {
-            ctx.drawImage(this.#videoEl, 0, 0, width, height);
-            // let pixels = ctx.getImageData(0, 0, width, height);
-            // ctx.putImageData(pixels, 0, 0);
-        }, 16)
+        ctx.drawImage(this.#videoEl, 0, 0, width, height);
+        let pixels = ctx.getImageData(0, 0, width, height);
+        pixels = this.setColor(pixels);
+        ctx.putImageData(pixels, 0, 0);
+
+        requestAnimationFrame(this.setIntoCanvas.bind(this, canvas));
     }
 
     takePhoto(canvas: HTMLCanvasElement) {
@@ -53,25 +61,105 @@ class WebCam {
     }
 
     getColorInput() {
+        const rgbContainer = document.querySelector('.rgb') as HTMLDivElement;
+        rgbContainer.querySelectorAll('input').forEach(input => {
+            const elName = input.name as 'temp' | 'tint';
+            const val = Number(input.value);
+            this.#rgbState[elName] = val;
+        })
 
+        const filter = document.querySelector('.imgFilter') as HTMLFieldSetElement;
+        filter.querySelectorAll('input').forEach(input => {
+            if (!input.checked) return
+            const val = input.value;
+            this.#rgbState.filter = val;
+        })
     }
 
-    setColorInput() {
+    adjustState(r: number, g: number, b: number) {
+        const { temp, tint } = this.#rgbState;
+        const newR = r + temp * 50;
+        const newB = b - temp * 50;
+        const newG = g + tint * 50;
+        return [
+            Math.max(0, Math.min(255, newR)),
+            Math.max(0, Math.min(255, newG)),
+            Math.max(0, Math.min(255, newB))
+        ];
+    }
 
+    grayScale(imageData: ImageData) {
+        for (let i = 0; i < imageData.data.length; i += 4) {
+            const red = imageData.data[i];
+            const green = imageData.data[i + 1];
+            const blue = imageData.data[i + 2];
+
+            const avg = (red + green + blue) / 3;
+
+            imageData.data[i] = avg;     // Red
+            imageData.data[i + 1] = avg; // Green
+            imageData.data[i + 2] = avg; // Blue
+        }
+    }
+
+    inverted(imageData: ImageData) {
+        for (let i = 0; i < imageData.data.length; i += 4) {
+            imageData.data[i] = 255 - imageData.data[i];       // Red
+            imageData.data[i + 1] = 255 - imageData.data[i + 1]; // Green
+            imageData.data[i + 2] = 255 - imageData.data[i + 2]; // Blue
+        }
+    }
+
+    applyFilter(imageData: ImageData) {
+        switch (this.#rgbState.filter) {
+            case 'grayscale':
+                this.grayScale(imageData);
+                break;
+
+            case 'inverted':
+                this.inverted(imageData);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    setColor(target: ImageData) {
+        this.getColorInput();
+
+        for (let i = 0; i < target.data.length; i += 4) {
+            const red = target.data[i];
+            const green = target.data[i + 1];
+            const blue = target.data[i + 2];
+
+            const [newR, newG, newB] = this.adjustState(red, green, blue);
+
+            target.data[i] = newR;
+            target.data[i + 1] = newG;
+            target.data[i + 2] = newB;
+
+        }
+        this.applyFilter(target);
+
+        return target
     }
 }
 
-// const Cam = new WebCam('.player');
+const Cam = new WebCam('.player');
 
 video.addEventListener('loadedmetadata', () => {
-    // Cam.setIntoCanvas(canvas);
+    Cam.setIntoCanvas(canvas);
 })
 
 function takePhoto() {
     // Cam.takePhoto(canvas);
 }
 
+const rgbContainer = document.querySelector('.rgb') as HTMLDivElement;
+console.dir(rgbContainer);
 /**
  * 實作功能
- * https://developer.mozilla.org/zh-CN/docs/Web/API/Canvas_API/Tutorial/Pixel_manipulation_with_canvas
- */
+ * webcam投影
+ * 並改變影像輸出
+*/
